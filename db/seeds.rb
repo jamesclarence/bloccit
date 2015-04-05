5e1c3ef437b399ce34908a1675695f99f13bb7a3
James Fisher <fisher.jc@gmail.com>
2015-04-05 17:29:54 -0500

    Create controller and update Views for Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,21 @@ require 'faker'
 
 # Create Posts
 50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-    )
+ Post.create!(
+   title:  Faker::Lorem.sentence,
+   body:   Faker::Lorem.paragraph
+ )
 end
-post = Post.all?
+posts = Post.all
 
 # Create Comments
 100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
+ Comment.create!(
+   post: posts.sample,
+   body: Faker::Lorem.paragraph
+ )
 end
 
 puts "Seed finished"
-puts "#{Post.count} post created"
+puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
