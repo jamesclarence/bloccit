8e7f720f59b1f26e10307536ca0f9c30b963c138
James Fisher <fisher.jc@gmail.com>
2015-04-29 17:44:57 -0500

    uploading images checkpoint complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423002430) do
+ActiveRecord::Schema.define(version: 20150427023319) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
