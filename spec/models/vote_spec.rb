ad885cfd5e969a7fe4546c224e50daf4b6fd1edf
James Fisher <fisher.jc@gmail.com>
2015-05-08 21:49:23 -0500

    complete intro to rspec assignment

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      if "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+        expect( @vote.validations ).to eq(true)
+
+        v2 = Vote.new(value: -1)
+        expect( @vote.validations ).to eq(true)
+
+        v3 = Vote.new(value: 2)
+        expect( @vote.validations ).to eq(false)
+        end
+    end
+  end
+end
