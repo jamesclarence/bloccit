de26cc5fca97c3169b26ed8c4ea1a4a0339d9f91
James Fisher <fisher.jc@gmail.com>
2015-05-10 11:57:40 -0500

    voting checkpoint completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :posts 
-  has_many :comments
+  has_many :posts, dependent: :destroy 
+  has_many :comments, dependent: :destroy 
   mount_uploader :avatar, AvatarUploader
+  has_many :votes, dependent: :destroy 
 
   def admin?
     role == 'admin'
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,16 +3,17 @@ require 'rails_helper'
 describe Vote do
   describe "validations" do
     describe "value validation" do
-      if "only allows -1 or 1 as values" do
+      it "only allows -1 or 1 as values" do
         v = Vote.new(value: 1)
-        expect( @vote.validations ).to eq(true)
+        expect(v.valid?).to eq(true)
 
         v2 = Vote.new(value: -1)
-        expect( @vote.validations ).to eq(true)
+        expect(v2.valid?).to eq(true)
 
-        v3 = Vote.new(value: 2)
-        expect( @vote.validations ).to eq(false)
-        end
+        bad_v = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
+      end
     end
   end
 end
+
