3762985a014ba5e3efc435a92971d3a4ac0087dc
James Fisher <fisher.jc@gmail.com>
2015-05-18 13:03:43 -0500

    another interlude checkpoint, commit 1

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
   
-  resources :posts, only: [] do 
+  resources :posts, only: [:index] do 
       resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy]
 
